bb4785a735394738ba8d2b42099b807f22e9f7d8
Sergey Movsesyan <sm877141@gmail.com>
2022-11-10 21:05:35 +0100

    added Style Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import "./App.css";
+import styled from "styled-components"
 
 function App() {
   return (
-    <div className="App">
-      <h1>React and Morty</h1>
-      <p>Morty Smith</p>
+    <Main>
+      <Header><i>React and Morty</i></Header>
+      <Card>
+      <h2>Morty Smith</h2>
       <img
         alt="rick"
         src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
       />
-      <footer>
-      <nav>
+      </Card>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Navbar>
         <ul>
-          <li><a>Home</a></li>
-          <li><a>Favorits</a></li>
-          <li><a>Information</a></li>
-          <li><a>Kontakt</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href = "#">Favorits</a></li>
+          <li><a href = "#">Information</a></li>
+          <li><a href = "#">Kontakt</a></li>
         </ul>
-      </nav>
-      </footer>
-    </div>
+      </Navbar>
+    </Main>
   );
 }
+const Main = styled.main`
+display:flex;
+flex-direction:column;
+align-items:center;
+text-align:center;
+`
+
+const Header = styled.h1`
+color:coral;
+font-size:60px;
+width:100%;
+font-family: Times New Roman;
+background-color:skyblue;
+background-width:100%;
+margin:0;
+`
+const Card = styled.div`
+margin-top:20px;
+border:5px solid orange;
+border-radius: 15px 15px;
+width:350px;
+
+img{
+  transition:2s;
+}
+
+img:hover {
+  background-color:black;
+  transform:scaleX(-1)
+}
+`
+const Navbar = styled.nav`
+width:100%;
+background-color:skyblue;
+ul{background-color:white;
+  display:flex;
+  justify-content:space-around;
+  list-style-type:none;
+}
+a{
+  transition:2s;
+  text-decoration:none;
+  color:coral;
+}
+a:hover{
+  box-shadow: 4px 4px 4px 4px wheat;
+  font-size:20px;
+}
+`
 export default App;
